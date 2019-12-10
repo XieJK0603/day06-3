47124e83b62dfbbe2af6de10992fda0b49c25460
XJK <xiejk_86765415@163.com>
2019-12-10 17:55:53 +0800

    rawd

--- run_suite.py
+++ run_suite.py
@@ -1,6 +1,7 @@
 # 导包
 import unittest
 from tools.HTMLTestReportCN import HTMLTestRunner
+
 # 定义测试套件
 suite = unittest.defaultTestLoader.discover("./scripts")
 # 获取报告存储文件流 并 实例化HtmlTestRunner 调用run方法
